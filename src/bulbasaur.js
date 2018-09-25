a7168a507353da585af74abf0f589eea39971b54
jordanstholmes <jordanstholmes@gmail.com>
2018-09-25 11:37:52 -0700

    Created dancing functionality for bulbasaur

--- src/bulbasaur.js
+++ src/bulbasaur.js
@@ -1,30 +1,50 @@
 var makeBulbasaurDancer = function(top, left, timeBetweenSteps) {
-  this.$node = $('<img src="assets/bulbasaur.png" class="dancer charmander">');
+  // this = 
+  this.$node = $('<img src="assets/bulbasaur.png" class="dancer bulbasaur">');
   makeDancer.apply(this, arguments);
-  this.wiggle();
+  this.spin();
 };
 
 makeBulbasaurDancer.prototype = Object.create(makeDancer.prototype);
 makeBulbasaurDancer.prototype.constructor = makeBulbasaurDancer;
 
-makeBulbasaurDancer.prototype.wiggle = function() {
-  var degree = 0;
-  var direction = 1;
-  var thisElement = this.$node;
+makeBulbasaurDancer.prototype.spin = function() {
+  var degree = 0; // a counter
+  var thisElement = this.$node; // this = the new bulbasaur object 
 
-  let rotate = function () {
-    thisElement.css({WebkitTransform: 'rotate(' + degree + 'deg)'});
-    setTimeout(function() {
-      if(degree >= 10) {
-        direction = -1;
-      } else if (degree <= -10) {
-        direction = 1;
-      }
+  let rotate = function () { // defining rotate function
+    thisElement.css({WebkitTransform: 'rotate(' + degree + 'deg)'}); //set rotation to 'degree' degrees
 
-      degree+= direction;
-      rotate();
+    setTimeout(function() { // after 10 miliseconds, run this function
+      degree++; // increase the rotation clockwise
+      rotate(); // recursively call rotate
     }, 10);
+
   };
 
-  rotate();
+  rotate(); // start the rotate recursion
 };
+
+
+
+// makeBulbasaurDancer.prototype.wiggle = function() {
+//   var degree = 0;
+//   var direction = 1;
+//   var thisElement = this.$node;
+
+//   let rotate = function () {
+//     thisElement.css({WebkitTransform: 'rotate(' + degree + 'deg)'});
+//     setTimeout(function() {
+//       if(degree >= 10) {
+//         direction = -1;
+//       } else if (degree <= -10) {
+//         direction = 1;
+//       }
+
+//       degree+= direction;
+//       rotate();
+//     }, 10);
+//   };
+
+//   rotate();
+// };
